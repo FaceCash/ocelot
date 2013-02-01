e6761b7d86233fe1e13610a238b8c3d48f9e781e
Matheus Lima <matheus.se@gmail.com>
2013-02-01 16:56:48 -0300

    add Campaing and Like

---
+++ ocelot/ocelot/app/facebook/migrations/0002_auto__add_like__add_campaign.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Like'
+        db.create_table('facebook_like', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
+            ('campaign', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['facebook.Campaign'])),
+            ('facebook_user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fandjango.User'])),
+        ))
+        db.send_create_signal('facebook', ['Like'])
+
+        # Adding model 'Campaign'
+        db.create_table('facebook_campaign', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True, db_index=True)),
+            ('facebook_id', self.gf('django.db.models.fields.BigIntegerField')()),
+        ))
+        db.send_create_signal('facebook', ['Campaign'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Like'
+        db.delete_table('facebook_like')
+
+        # Deleting model 'Campaign'
+        db.delete_table('facebook_campaign')
+
+
+    models = {
+        'facebook.campaign': {
+            'Meta': {'object_name': 'Campaign'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'facebook_id': ('django.db.models.fields.BigIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'likers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'Campaigns'", 'symmetrical': 'False', 'through': "orm['facebook.Like']", 'to': "orm['fandjango.User']"}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'facebook.like': {
+            'Meta': {'object_name': 'Like'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
+            'campaign': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['facebook.Campaign']"}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'facebook_user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['fandjango.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'fandjango.oauthtoken': {
+            'Meta': {'object_name': 'OAuthToken'},
+            'expires_at': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'issued_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'token': ('django.db.models.fields.TextField', [], {})
+        },
+        'fandjango.user': {
+            'Meta': {'object_name': 'User'},
+            'authorized': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'birthday': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'facebook_id': ('django.db.models.fields.BigIntegerField', [], {'unique': 'True'}),
+            'facebook_username': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'last_seen_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'oauth_token': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['fandjango.OAuthToken']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['facebook']
--- ocelot/ocelot/app/facebook/models.py
+++ ocelot/ocelot/app/facebook/models.py
@@ -1,3 +1,30 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 
-# Create your models here.
+from ocelot.app.core.models import DefaultFields
+
+
+
+class Campaign(DefaultFields):
+    facebook_id = models.BigIntegerField(verbose_name=_('facebook id'))
+    likers = models.ManyToManyField('fandjango.User', through='Like', related_name='Campaigns')
+    owner = models.ForeignKey('fandjango.User', related_name=_('owner'))
+
+    class Meta:
+        verbose_name = _('Campaign')
+        verbose_name_plural = _('Campaigns')
+
+    def __unicode__(self):
+        pass
+
+
+class Like(DefaultFields):
+    campaign = models.ForeignKey('Campaign', verbose_name=_('campaign'))
+    facebook_user = models.ForeignKey('fandjango.User', verbose_name=_('facebook user'))
+
+    class Meta:
+        verbose_name = _('Like')
+        verbose_name_plural = _('Likes')
+
+    def __unicode__(self):
+        pass
