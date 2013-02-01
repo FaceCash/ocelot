867f746cb0399d6c38e61b3b2babcf78b3569eae
Matheus Lima <matheus.se@gmail.com>
2013-02-01 15:48:49 -0300

    add fandjango and facebook apps to settings

--- ocelot/ocelot/settings/settings.py
+++ ocelot/ocelot/settings/settings.py
@@ -115,6 +115,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fandjango.middleware.FacebookMiddleware',
 )
 
 ROOT_URLCONF = 'ocelot.urls'
@@ -151,12 +152,12 @@ INSTALLED_APPS = (
 
     # third party apps
     'south',
-    'bootstrap',
     'dajaxice',
     'dajax',
-    'tastypie',
+    'fandjango',
     
     # our apps
+    'ocelot.app.facebook',
 
 )
 
